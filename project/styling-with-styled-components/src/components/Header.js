e81814bd586a5affcb9d4812b40866ed73811a51
jci501 <30316030+jci501@users.noreply.github.com>
2021-10-10 19:21:05 +0900

    feat: 웹 퍼블리싱 첫 화면 진행

--- project/styling-with-styled-components/src/components/Header.js
+++ project/styling-with-styled-components/src/components/Header.js
@@ -4,12 +4,32 @@ const StyledDiv = styled.div`
  width: 100%;
  height: 411px;
 `;
+const Title= styled.p`
+  color:  #23672A;
+  font-size: 20px;
+  font-weight: 600;
+  line-height: 1.0;
+`;
+const Paragraph = styled.h1`
+  color: #EA6327; 
+  font-size: 50px;
+  font-weight: 600;
+`;
+const Para = styled.h1`
+  color: #EA6327; 
+  font-size: 20px;
+  font-weight: 600;
+`;
+
 export default function Header() {
    
     return (
+
 <StyledDiv>
-    <p>인생제주</p>
-<img className="Image" alt="header_01" src="../img/header_img.png" />
+    <Title>COTRIP</Title>
+    <Para>쉬고 싶으면? 제주! 인생 사진도? 제주!</Para>
+    <Paragraph>인생제주</Paragraph>
+<img className="Image" alt="header_01" src="img/header_img.png" />
 </StyledDiv>
        
     );
