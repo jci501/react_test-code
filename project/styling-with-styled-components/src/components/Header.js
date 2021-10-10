c95a86424dc9c9185ad83e6609e980239ccbfc37
jci501 <30316030+jci501@users.noreply.github.com>
2021-10-10 20:20:28 +0900

    feat: 첫 화면 웹 퍼블리싱

--- project/styling-with-styled-components/src/components/Header.js
+++ project/styling-with-styled-components/src/components/Header.js
@@ -1,24 +1,37 @@
 import styled from 'styled-components';
+
 const StyledDiv = styled.div`
  background-color: #FEF8EA;
  width: 100%;
- height: 411px;
+ height: 800px;
 `;
 const Title= styled.p`
   color:  #23672A;
   font-size: 20px;
-  font-weight: 600;
+  font-weight: 700;
   line-height: 1.0;
+  padding-top: 3rem;
+  padding-right: 25rem;
+  
 `;
-const Content1 = styled.h1`
+const Content0 = styled.h1`
+  color: #FCD937; 
+  font-size: 50px;
+  font-weight: 600;
+  padding-right: 15rem;
+  font-family:'RockSalt-Regular';
+`;
+const Content1 = styled.h2`
   color: #EA6327; 
   font-size: 20px;
   font-weight: 600;
+  padding-left: 10rem;
 `;
 const Content2 = styled.h1`
   color: #EA6327; 
-  font-size: 50px;
+  font-size: 70px;
   font-weight: 600;
+  padding-left: 5rem;
 `;
 
 export default function Header() {
@@ -27,6 +40,7 @@ export default function Header() {
 
 <StyledDiv>
     <Title>COTRIP</Title>
+    <Content0>JEJu</Content0>
     <Content1>쉬고 싶으면? 제주! 인생 사진도? 제주!</Content1>
     <Content2>인생제주</Content2>
 <img className="Image" alt="header_01" src="img/header_img.png" />
--- project/styling-with-styled-components/src/components/Maincontent.js
+++ project/styling-with-styled-components/src/components/Maincontent.js
@@ -3,11 +3,25 @@ const StyledDiv = styled.div`
  background-color: #FEF2C6;
  width: 100%;
  height: 411px;
+ float : left;
 `;
+const Title= styled.h1`
+  color:  #518B0E;
+  font-size: 20px;
+  font-weight: 600;
+`;
+const SubTitle= styled.h1`
+  color:  black;
+  font-size: 20px;
+  font-weight: 600;
+`;
+
 export default function Header() {
    
     return (
+        
         <StyledDiv>
+            <Title>제주 쉬기좋은</Title><SubTitle>지역별 안내</SubTitle>
             <img className="Image" alt="header_01" src="img/main_content_img1.png" />
         </StyledDiv>
 
